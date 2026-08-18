8957b2353711811ada512364b1c69f9e21116e8d
kacper7-7 <kogutkacper7@gmail.com>
2026-08-18 20:22:38 +0200

    feat: add str methods for models

--- core/models.py
+++ core/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class Tag(models.Model):
     name = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.name
+
 
 class Task(models.Model):
     content = models.CharField(max_length=255)
@@ -11,3 +14,6 @@ class Task(models.Model):
     deadline_datetime = models.DateTimeField()
     is_completed = models.BooleanField(default=False)
     tags = models.ManyToManyField(Tag, blank=True, related_name="tasks")
+
+    def __str__(self):
+        return self.content
